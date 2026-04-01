861dc8f18d9ed1546cc225a6144bd299222191f4
saravanankandasamyGKS <gksaravanan008@gmail.com>
2026-04-01 13:00:54 +0530

    update1

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { userAuth } = require("../middlewares/auth");
+const { userAuth } = require("../Middlewares/auth");
 
 const chatRouter = express.Router();
 
