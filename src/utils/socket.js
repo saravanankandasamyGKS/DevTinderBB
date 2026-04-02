c07949994a1923f4b78344be4a004b53d2126324
saravanankandasamyGKS <gksaravanan008@gmail.com>
2026-04-02 13:23:34 +0530

    updation

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,7 @@
 const socket = require("socket.io");
 const crypto = require("crypto");
-const { Chat } = require("../models/chat");
-const ConnectionRequest = require("../models/connectionRequest");
+const { Chat } = require("../Models/chat");
+const ConnectionRequest = require("../Models/connectionRequest");
 
 const getSecretRoomId = (userId, targetUserId) => {
   return crypto
